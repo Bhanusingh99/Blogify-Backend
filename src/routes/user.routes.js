466dd735929d742527acb9c24e6ac744c8d90eaa
Bhanu <bhanusngh12345@gmail.com>
2024-02-21 10:38:03 +0530

    some changes in backend code

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import  signUp ,{logIn} from '../controllers/auth.controller.js';
 
 const userRouter = Router();
-userRouter.post('/sign-up', signUp);
-userRouter.post('/log-in',logIn);
+
 
 export default userRouter;
